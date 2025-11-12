6964506a4e161e97aad5c3aaebb460c8aa3e368a
WTamiyanon <w.tamiyanon@gmail.com>
2025-11-12 22:56:28 +0700

    initial commit: ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+/* eslint-disable react/react-in-jsx-scope */
+import { useEffect, useState } from "react";
 import './App.css';
+import searchIcon from './search.svg';
+import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// dec0a434
+
+const API_URL = 'https://www.omdbapi.com?apikey=dec0a434';
+const DEFAULT_INITIAL_QUERY = 'Movie';
+
+const App = () => {
+    const [ movies, setMovies ] = useState ([]);
+    const [ searchTerm, setSearchTerm ] = useState ('');
+    const [ isLoading, setIsLoading ] = useState(false);
+
+    const searchMovies = async (title) => {
+        const query = title.trim() === '' ? DEFAULT_INITIAL_QUERY : title;
+
+        setIsLoading(true);
+        try {
+            const response = await fetch(`${API_URL}&s=${query}`);
+            const data = await response.json();
+
+            setMovies(data.Search || []);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            setMovies([]);
+        } finally {
+            setIsLoading(false);
+        }
+        
+    };
+    useEffect(() => {
+        searchMovies(searchTerm);
+    }, [searchTerm]);
+    return (
+        <div className="app">
+            <h1>MovieLander</h1>
+            <div className="search">
+                <input 
+                    placeholder="Search for movie"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            searchMovies(searchTerm);
+                        }
+                    }}
+                />
+                <img
+                    src={searchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {isLoading && (
+                <div className="loading">
+                    <h2>Loading...</h2>
+                </div>
+            )}
+
+            { !isLoading && (
+                movies && movies?.length > 0 ? (
+                    <div className="container">
+                        { movies.map((movie) => ( 
+                            <MovieCard key={movie.imdbID} movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            )}
+
+        </div>
+    );
 }
 
 export default App;
